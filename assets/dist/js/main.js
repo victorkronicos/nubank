0ae99300a04dcd545722073c4e551137ac2facf2
Victor <victor_henrique@live.com>
2020-02-03 09:53:18 -0300

    create main screen

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,10 +1,52 @@
-var HamburguerMenu = document.querySelector('#hamburguer-menu');
-var MenuItems = document.querySelector('#toggle-menu');
+const HamburguerMenu = document.querySelector('#hamburguer-menu');
+const MenuItems = document.querySelector('#toggle-menu');
+const MainCards = document.querySelector('#main-cards');
+const BottomCards = document.querySelector('#bottom-menu');
+
+// Função para esconder o Menu Principal
+function CardPrincipal (){
+  MainCards.classList.toggle('hide');
+};
+
+function CardsBottom (){
+  BottomCards.classList.toggle('hide');
+}
+
 
 HamburguerMenu.addEventListener('click', function(e) {
   MenuItems.classList.toggle('show');
+  CardPrincipal();
+  CardsBottom();
   e.stopPropagation();
 });
 
 
 
+
+// Utilizando jQuery para fazer o Slider
+
+  $('.main-cards').slick({
+    dots: true,
+    infinite: false,
+    speed: 150,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    arrows: false,
+  });
+
+
+  // Utlizando jQuery para fazer o Bottom Slider
+
+  $('#slick-bottom').slick({
+    slide: 'li',
+    dots: false,
+    autoplay: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+  });
--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -1,8 +1,22 @@
-var HamburguerMenu = document.querySelector('#hamburguer-menu');
-var MenuItems = document.querySelector('#toggle-menu');
+const HamburguerMenu = document.querySelector('#hamburguer-menu');
+const MenuItems = document.querySelector('#toggle-menu');
+const MainCards = document.querySelector('#main-cards');
+const BottomCards = document.querySelector('#bottom-menu');
+
+// Função para esconder o Menu Principal
+function CardPrincipal (){
+  MainCards.classList.toggle('hide');
+};
+
+function CardsBottom (){
+  BottomCards.classList.toggle('hide');
+}
+
 
 HamburguerMenu.addEventListener('click', function(e) {
   MenuItems.classList.toggle('show');
+  CardPrincipal();
+  CardsBottom();
   e.stopPropagation();
 });
 
